6a4da801693782bf2d0fcbd520594bc3aa88d28c
Arthur Langereis <puurtuur@me.com>
2017-12-04 18:51:43 +0100

    functional grid puzzle

--- src/gridpillar.ts
+++ src/gridpillar.ts
@@ -1,10 +1,42 @@
+interface GridButan {
+	index: number;
+	comp: EntityInfo;
+	pressed: boolean;
+}
+
 class GridPillar implements Interactable {
-	private butans = new Map<entity.Entity, EntityInfo>();
+	private butans = new Map<entity.Entity, GridButan>();
+	private solved = false;
+	private correct = [
+		1, 0, 0,
+		0, 1, 0,
+		1, 1, 1
+	];
+
+	private pattern() {
+		const p: number[] = [];
+		this.butans.forEach(butan => {
+			p[butan.index] = +butan.pressed;
+		});
+		return p;
+	}
+
+	private checkPattern() {
+		const pattern = this.pattern();
+		if (pattern.every((v, i) => this.correct[i] === v)) {
+			this.solved = true;
+			this.gameState.solvedGrid();
+		}
+	}
 
 	hover(ent: entity.Entity) {
+		if (this.solved) {
+			return false;
+		}
+
 		const butan = this.butans.get(ent);
 		if (butan) {
-			const mats = this.scene.renderers.materials(butan.renderer) as effect.StandardEffectData[];
+			const mats = this.scene.renderers.materials(butan.comp.renderer) as effect.StandardEffectData[];
 			vec4.copy(mats[0].emissiveFactor, [0.2, 0.2, 0.2, 1]);
 			return true;
 		}
@@ -14,7 +46,7 @@ class GridPillar implements Interactable {
 	blur(ent: entity.Entity) {
 		const butan = this.butans.get(ent);
 		if (butan) {
-			const mats = this.scene.renderers.materials(butan.renderer) as effect.StandardEffectData[];
+			const mats = this.scene.renderers.materials(butan.comp.renderer) as effect.StandardEffectData[];
 			vec4.copy(mats[0].emissiveFactor, [0, 0, 0, 0]);
 			return true;
 		}
@@ -24,16 +56,24 @@ class GridPillar implements Interactable {
 	interact(ent: entity.Entity) {
 		const butan = this.butans.get(ent);
 		if (butan) {
-			this.scene.transforms.translate(butan.transform, [0, 0.04, 0]);
+			if (this.solved) {
+				this.gameState.showMessage("The switches are stuck in place.");
+			}
+			else {
+				butan.pressed = !butan.pressed;
+				const pos = this.scene.transforms.localPosition(butan.comp.transform);
+				this.scene.transforms.setPosition(butan.comp.transform, [pos[0], butan.pressed ? 0.04 : 0.07, pos[2]]);
+				this.checkPattern();
+			}
 			return true;
 		}
 		return false;
 	}
 
-	constructor(public scene: sd.Scene, public cache: asset.CacheAccess, public sound: Sound) {
-		const lite = makePBRMat(scene, cache("material", "Metal_Silver"));
+	constructor(public gameState: GameState, public scene: sd.Scene, public cache: asset.CacheAccess, public sound: Sound) {
 		const dark = makePBRMat(scene, cache("material", "Granite_Dark_Gray"));
 
+		// pillar
 		makeEntity(scene, {
 			transform: {
 				position: [-24.5, 3.32, 32],
@@ -47,13 +87,15 @@ class GridPillar implements Interactable {
 			}
 		});
 
+		const SLAB_DIM = 0.8;
+		const HALF_SLAB_DIM = SLAB_DIM / 2;
 		const slab = makeEntity(scene, {
 			transform: {
 				position: [-24.5, 3.8, 32],
-				rotation: quat.fromEuler(Math.PI, math.deg2rad(325), math.deg2rad(215))
+				rotation: quat.fromEuler(Math.PI, math.deg2rad(325), math.deg2rad(230))
 			},
 			geom: geometry.gen.generate(new geometry.gen.Box({
-				width: 1, height: 0.1, depth: 1
+				width: SLAB_DIM, height: 0.1, depth: SLAB_DIM 
 			})),
 			renderer: {
 				materials: [dark]
@@ -67,31 +109,43 @@ class GridPillar implements Interactable {
 			}
 		});
 
-		const oos = 1 / 7;
+		const oos = SLAB_DIM  / 7;
 		const hoos = oos / 2;
 		const buttonShape = physics.makeShape({
 			type: physics.PhysicsShapeType.Box,
-			halfExtents: [oos, .04, oos]
+			halfExtents: [oos, .02, oos]
 		})!;
 
-		const butan = makeEntity(scene, {
-			parent: slab.transform,
-			transform: {
-				position: [-.5 + hoos + oos, .08, -.5 + hoos + oos]
-			},
-			geom: geometry.gen.generate(new geometry.gen.Box({
-				width: oos, height: .08, depth: oos
-			})),
-			renderer: {
-				materials: [lite]
-			},
-			rigidBody: {
-				mass: 0,
-				shape: buttonShape,
-				isTrigger: true,
-				isKinematic: true
-			}
-		});
-		this.butans.set(butan.entity, butan);
+		const makeButan = (index: number) => {
+			const gx = (index % 3) * 2;
+			const gy = ((index / 3) >> 0) * 2;
+			const butan = makeEntity(scene, {
+				parent: slab.transform,
+				transform: {
+					position: [-HALF_SLAB_DIM + hoos + oos + (gx * oos), .07, -HALF_SLAB_DIM + hoos + oos + (gy * oos)]
+				},
+				geom: geometry.gen.generate(new geometry.gen.Box({
+					width: oos, height: .04, depth: oos
+				})),
+				renderer: {
+					materials: [makePBRMat(scene, cache("material", "Metal_Silver"))]
+				},
+				rigidBody: {
+					mass: 0,
+					shape: buttonShape,
+					isTrigger: true,
+					isKinematic: true
+				}
+			});
+			this.butans.set(butan.entity, {
+				index,
+				comp: butan,
+				pressed: false
+			});
+		};
+
+		for (let bx = 0; bx < 9; ++bx) {
+			makeButan(bx);
+		}
 	}
 }
